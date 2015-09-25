6b8f77891ad9be3de478210a0ed6632963cbb898
EnzoVezzaro <enzo-vezzaro@live.it>
2015-09-25 11:31:31 -0700

    update to responsive

--- js/main.js
+++ js/main.js
@@ -4,126 +4,287 @@
 
 console.log("Look at app/js/main.js");
 
-//onclick activation super-nintendo
-$(".super-nintendo").click(function () {
-  var nameClass = "";
-  if ($(this).hasClass("super-nintendo")) {
-    nameClass = "super-nintendo-open";
-    $(this).removeClass("super-nintendo");
-  } else {
-    nameClass = "super-nintendo";
-    $(this).removeClass("super-nintendo-open");
-  }
-  $(this).addClass(nameClass);
-  if ($(this).hasClass("super-nintendo")) {
-    $(".playstation").show();
-    $(".sega").show();
-    $(".nintendo-64").show();
+var mql = window.matchMedia("@media screen and (min-width: 1200px)");
+if (window.matchMedia("(min-width: 1200px)").matches) {
+  // the width of browser is more then 1200px
+  //onclick activation super-nintendo
+  $(".super-nintendo").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("super-nintendo")) {
+      nameClass = "super-nintendo-open";
+      $(this).removeClass("super-nintendo");
+    } else {
+      nameClass = "super-nintendo";
+      $(this).removeClass("super-nintendo-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("super-nintendo")) {
+      $(".playstation").show();
+      $(".sega").show();
+      $(".nintendo-64").show();
+      $(".super-text").css("display", "none");
+      $(".out-menu").css("position", "");
+    } else {
+      $(".playstation").hide();
+      $(".sega").hide();
+      $(".nintendo-64").hide();
+      $(".super-text").css("display", "block");
+      $(".out-menu").css("position", "fixed");
+    };
+  });
+
+  //onclick activetion playstation
+  $(".playstation").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("playstation")) {
+      nameClass = "playstation-open";
+      $(this).removeClass("playstation");
+    } else {
+      nameClass = "playstation";
+      $(this).removeClass("playstation-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("playstation")) {
+      $(".super-nintendo").show();
+      $(".sega").show();
+      $(".nintendo-64").show();
+      $(".playstation-text").css("display", "none");
+      $(".out-menu").css("position", "");
+    } else {
+      $(".super-nintendo").hide();
+      $(".sega").hide();
+      $(".nintendo-64").hide();
+      $(".playstation-text").css("display", "block");
+      $(".out-menu").css("position", "fixed");
+    };
+  });
+
+  //onclick activetion sega
+  $(".sega").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("sega")) {
+      nameClass = "sega-open";
+      $(this).removeClass("sega");
+    } else {
+      nameClass = "sega";
+      $(this).removeClass("sega-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("sega")) {
+      $(".super-nintendo").show();
+      $(".playstation").show();
+      $(".nintendo-64").show();
+      $(".sega-text").css("display", "none");
+      $(".out-menu").css("position", "");
+    } else {
+      $(".super-nintendo").hide();
+      $(".playstation").hide();
+      $(".nintendo-64").hide();
+      $(".sega-text").css("display", "block");
+      $(".out-menu").css("position", "fixed");
+    };
+  });
+
+  //onclick activetion nintendo-64
+  $(".nintendo-64").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("nintendo-64")) {
+      nameClass = "nintendo-64-open";
+      $(this).removeClass("nintendo-64");
+    } else {
+      nameClass = "nintendo-64";
+      $(this).removeClass("nintendo-64-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("nintendo-64")) {
+      $(".super-nintendo").show();
+      $(".playstation").show();
+      $(".sega").show();
+      $(".nintendo-64-text").css("display", "none");
+      $(".out-menu").css("position", "");
+    } else {
+      $(".super-nintendo").hide();
+      $(".playstation").hide();
+      $(".sega").hide();
+      $(".nintendo-64-text").css("display", "block");
+      $(".out-menu").css("position", "fixed");
+    };
+  });
+
+  //menu
+  $(".icon").click(function () {
+    $(".out-menu").css("transform", "translateX(0px)");
+    $(".out-menu").css("width", "50vh");
+    $(".out-menu").css("height", "100vh");
+    $(".icon-container").hide();
+    $(".menu-text").css("display", "block");
+  });
+
+  $(".out-menu").mouseleave(function () {
+    $(".out-menu").css("transform", "translateX(-500px)");
+    $(".out-menu").css("width", "0vh");
+    $(".out-menu").css("height", "0vh");
+    $(".icon-container").show();
+    $(".menu-text").hide();
+  });
+} else if (window.matchMedia("(min-width: 1100px) and (max-width: 1200px)").matches) {
+  // the width of browser is less then 1200px
+  //onclick activation super-nintendo
+  $(".super-nintendo").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("super-nintendo")) {
+      nameClass = "super-nintendo-open";
+      $(this).removeClass("super-nintendo");
+    } else {
+      nameClass = "super-nintendo";
+      $(this).removeClass("super-nintendo-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("super-nintendo")) {
+      $(".playstation").show();
+      $(".sega").show();
+      $(".nintendo-64").show();
+      $(".super-text").css("display", "none");
+      $(".out-menu").css("display", "flex");
+    } else {
+      $(".playstation").hide();
+      $(".sega").hide();
+      $(".nintendo-64").hide();
+      $(".super-text").css("display", "block");
+      $(".out-menu").hide();
+    };
+  });
+
+  //onclick activetion playstation
+  $(".playstation").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("playstation")) {
+      nameClass = "playstation-open";
+      $(this).removeClass("playstation");
+    } else {
+      nameClass = "playstation";
+      $(this).removeClass("playstation-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("playstation")) {
+      $(".super-nintendo").show();
+      $(".sega").show();
+      $(".nintendo-64").show();
+      $(".playstation-text").css("display", "none");
+      $(".out-menu").css("display", "flex");
+    } else {
+      $(".super-nintendo").hide();
+      $(".sega").hide();
+      $(".nintendo-64").hide();
+      $(".playstation-text").css("display", "block");
+      $(".out-menu").css("display", "none");
+    };
+  });
+
+  //onclick activetion sega
+  $(".sega").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("sega")) {
+      nameClass = "sega-open";
+      $(this).removeClass("sega");
+    } else {
+      nameClass = "sega";
+      $(this).removeClass("sega-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("sega")) {
+      $(".super-nintendo").show();
+      $(".playstation").show();
+      $(".nintendo-64").show();
+      $(".sega-text").css("display", "none");
+      $(".out-menu").css("display", "flex");
+    } else {
+      $(".super-nintendo").hide();
+      $(".playstation").hide();
+      $(".nintendo-64").hide();
+      $(".sega-text").css("display", "block");
+      $(".out-menu").css("display", "none");
+    };
+  });
+
+  //onclick activetion nintendo-64
+  $(".nintendo-64").click(function () {
+    var nameClass = "";
+    if ($(this).hasClass("nintendo-64")) {
+      nameClass = "nintendo-64-open";
+      $(this).removeClass("nintendo-64");
+    } else {
+      nameClass = "nintendo-64";
+      $(this).removeClass("nintendo-64-open");
+    };
+    $(this).addClass(nameClass);
+    if ($(this).hasClass("nintendo-64")) {
+      $(".super-nintendo").show();
+      $(".playstation").show();
+      $(".sega").show();
+      $(".nintendo-64-text").css("display", "none");
+      $(".out-menu").css("display", "flex");
+    } else {
+      $(".super-nintendo").hide();
+      $(".playstation").hide();
+      $(".sega").hide();
+      $(".nintendo-64-text").css("display", "block");
+      $(".out-menu").css("display", "none");
+    };
+  });
+} else if (window.matchMedia("(max-width: 1100px)").matches) {
+  //super
+  $(".super-nintendo").click(function () {
+    $(".super-text").css("display", "flex");
+    $(".arrow").css("display", "flex");
+  });
+
+  $(".arrow").click(function () {
     $(".super-text").css("display", "none");
-    $(".out-menu").css("position", "");
-  } else {
-    $(".playstation").hide();
-    $(".sega").hide();
-    $(".nintendo-64").hide();
-    $(".super-text").css("display", "block");
-    $(".out-menu").css("position", "fixed");
-  }
-});
+    $(".arrow").css("display", "none");
+  });
+  //playstation
+  $(".playstation").click(function () {
+    $(".playstation-text").css("display", "flex");
+    $(".arrow-play").css("display", "flex");
+  });
 
-//onclick activetion playstation
-$(".playstation").click(function () {
-  var nameClass = "";
-  if ($(this).hasClass("playstation")) {
-    nameClass = "playstation-open";
-    $(this).removeClass("playstation");
-  } else {
-    nameClass = "playstation";
-    $(this).removeClass("playstation-open");
-  }
-  $(this).addClass(nameClass);
-  if ($(this).hasClass("playstation")) {
-    $(".super-nintendo").show();
-    $(".sega").show();
-    $(".nintendo-64").show();
+  $(".arrow-play").click(function () {
     $(".playstation-text").css("display", "none");
-    $(".out-menu").css("position", "");
-  } else {
-    $(".super-nintendo").hide();
-    $(".sega").hide();
-    $(".nintendo-64").hide();
-    $(".playstation-text").css("display", "block");
-    $(".out-menu").css("position", "fixed");
-  }
-});
+    $(".arrow-play").css("display", "none");
+  });
+  //sega
+  $(".sega").click(function () {
+    $(".sega-text").css("display", "flex");
+    $(".arrow-sega").css("display", "flex");
+  });
 
-//onclick activetion sega
-$(".sega").click(function () {
-  var nameClass = "";
-  if ($(this).hasClass("sega")) {
-    nameClass = "sega-open";
-    $(this).removeClass("sega");
-  } else {
-    nameClass = "sega";
-    $(this).removeClass("sega-open");
-  }
-  $(this).addClass(nameClass);
-  if ($(this).hasClass("sega")) {
-    $(".super-nintendo").show();
-    $(".playstation").show();
-    $(".nintendo-64").show();
+  $(".arrow-sega").click(function () {
     $(".sega-text").css("display", "none");
-    $(".out-menu").css("position", "");
-  } else {
-    $(".super-nintendo").hide();
-    $(".playstation").hide();
-    $(".nintendo-64").hide();
-    $(".sega-text").css("display", "block");
-    $(".out-menu").css("position", "fixed");
-  }
-});
+    $(".arrow-sega").css("display", "none");
+  });
+  //nintendo-64
+  $(".nintendo-64").click(function () {
+    $(".nintendo-64-text").css("display", "flex");
+    $(".arrow-nintendo-64").css("display", "flex");
+  });
 
-//onclick activetion nintendo-64
-$(".nintendo-64").click(function () {
-  var nameClass = "";
-  if ($(this).hasClass("nintendo-64")) {
-    nameClass = "nintendo-64-open";
-    $(this).removeClass("nintendo-64");
-  } else {
-    nameClass = "nintendo-64";
-    $(this).removeClass("nintendo-64-open");
-  }
-  $(this).addClass(nameClass);
-  if ($(this).hasClass("nintendo-64")) {
-    $(".super-nintendo").show();
-    $(".playstation").show();
-    $(".sega").show();
+  $(".arrow-nintendo-64").click(function () {
     $(".nintendo-64-text").css("display", "none");
-    $(".out-menu").css("position", "");
-  } else {
-    $(".super-nintendo").hide();
-    $(".playstation").hide();
-    $(".sega").hide();
-    $(".nintendo-64-text").css("display", "block");
-    $(".out-menu").css("position", "fixed");
-  }
-});
+    $(".arrow-nintendo-64").css("display", "none");
+  });
 
-//menu
-$(".icon").click(function () {
-  $(".out-menu").css("transform", "translateX(0px)");
-  $(".out-menu").css("width", "50vh");
-  $(".out-menu").css("height", "100vh");
-  $(".icon-container").hide();
-  $(".menu-text").css("display", "block");
-});
+  //arrow menu
+  $(".arrow-menu").click(function () {
+    $(".out-menu").slideUp(1);
+    $(".icon-container").css("display", "flex");
+  });
 
-$(".out-menu").mouseleave(function () {
-  $(".out-menu").css("transform", "translateX(-500px)");
-  $(".out-menu").css("width", "0vh");
-  $(".out-menu").css("height", "0vh");
-  $(".icon-container").show();
-  $(".menu-text").hide();
-});
-
+  $(".icon").click(function () {
+    $(".out-menu").slideDown(1);
+  });
+};
 
 },{}]},{},[1]);
+
+//# sourceMappingURL=main.js.map
